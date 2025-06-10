c7a471e61e4030fd7c95ce827786b43d5a1c9144
Pedro-MartinezLimon <Pedrolimmon@gmail.com>
2025-06-10 15:52:06 -0600

    Agrego prototipo de Reproductor

---
+++ Dynamics/JS/Reproductor.js
@@ -0,0 +1,30 @@
+/* 
+ * Reproductor de musica 
+ * 
+ * En este archivo se encuentran los elementos basicos de un reproductor de musica: Creo una clase Canción y una clase Playlist
+ * Canción: Nombre, Genero, ID, Artista/Banda, URL
+ * Playlist: array_canciones
+ * 
+ * Pedro
+ * 10 de junio de 2025
+*/
+
+class Cancion
+{
+
+    constructor(Nombre, Genero, Artista, URL, ID)
+    {
+        this.Nombre = Nombre;
+        this.Genero = Genero;
+        this.Artista = Artista;
+        this.URL = URL;
+        this.ID = ID;
+    }
+
+    //Setters
+    set Nombre(nombre)
+    {
+        
+    }
+    //Getters
+}
